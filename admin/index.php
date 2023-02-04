b82453c9112d94a9f837324201faf33141abe6a5
Ringo Ebenezer <ringo.ebenezer@gmail.com>
2023-02-04 21:31:32 +0300

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suma's - Home Page</title>
+</head>
+
+<body>
+    <?php   include("../admin/partials/menu.php") ?>
+
+    <!-- Main Content -->
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Dashboard</h1>
+            <div class="col-4 text-center">
+                <h1>5</h1> <br>
+                Categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1> <br>
+                Categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1> <br>
+                Categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1> <br>
+                Categories
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+    <!-- Main Content End  -->
+
+    <?php include("../admin/partials/footer.php") ?>
+
+</body>
+
+</html>
---
+++ admin/manage-food.php
@@ -0,0 +1,7 @@
+<?php   include("../admin/partials/menu.php") ?>
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Manage Food</h1>
+    </div>
+</div>
+<?php include("../admin/partials/footer.php") ?>
