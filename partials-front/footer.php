1e2fa7f533a0b9b9a7f039f4e426133a19235148
Ringo Ebenezer <ringo.ebenezer@gmail.com>
2023-03-01 07:51:28 +0300

    static

---
+++ partials-front/footer.php
@@ -0,0 +1,28 @@
+<!-- social Section Starts Here -->
+<section class="social">
+    <div class="container text-center">
+        <ul>
+            <li>
+                <a href="https://twitter.com/ringo__jr"><img
+                        src="https://img.icons8.com/fluent/48/000000/twitter.png" /></a>
+            </li>
+            <li>
+                <a href="https://www.instagram.com/ringo_jr_/"><img
+                        src="https://img.icons8.com/fluent/48/000000/instagram-new.png" /></a>
+            </li>
+        </ul>
+    </div>
+</section>
+<!-- social Section Ends Here -->
+
+<!-- footer Section Starts Here -->
+<section class="footer">
+    <div class="container text-center">
+        <p>All rights reserved. Designed By <a href="https://github.com/ringoebenezer">Ringo Jr</a></p>
+    </div>
+</section>
+<!-- footer Section Ends Here -->
+
+</body>
+
+</html>
